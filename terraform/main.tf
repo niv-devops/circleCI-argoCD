7d79ec5c966a879ffab53c804225228b3cc02bd8
niv-devops <nivz267@gmail.com>
2024-11-21 10:34:59 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.75.0"
     }
   }
+  backend "s3" {
+    bucket = "tasty-kfc-bucket"
+    key    = "state/terraform.tfstate"
+    region = "eu-central-1"
+  }
   required_version = ">= 1.8.5"
 }
 
@@ -12,28 +17,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-}
-
-resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = var.az_a
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = var.az_b
-  map_public_ip_on_launch = true
-}
-
 module "eks" {
   source       = "github.com/terraform-aws-modules/terraform-aws-eks?ref=97a08c8aff5dbf51a86b4c8cd88a858336cd0208" # v20.29.0
   cluster_name = var.cluster_name
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = var.vpc
+  subnet_ids               = var.subnets
+  control_plane_subnet_ids = var.control_plane_subnet_ids
   eks_managed_node_groups  = var.node_groups
-  control_plane_subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
-  subnet_ids   = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 }
