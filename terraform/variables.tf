3c1b29c0f5825c4afab35892bd6e998f1627107d
niv-devops <nivz267@gmail.com>
2024-11-21 10:35:36 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,24 @@ variable "cluster_name" {
   default     = "eks-cluster"
 }
 
+variable "vpc" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = string
+  default     = "vpc-09305c0e6f4405f3e"
+}
+
+variable "subnets" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+  default     = ["subnet-02b535d84f8ef94fb", "subnet-0f3e375081ee0ba18"]
+}
+
+variable "control_plane_subnet_ids" {
+  description = "List of subnet IDs for the EKS control plane"
+  type        = list(string)
+  default     = ["subnet-02b535d84f8ef94fb", "subnet-0f3e375081ee0ba18"]
+}
+
 variable "node_groups" {
   description = "Map of EKS Node Groups"
   type = map(any)
@@ -19,10 +37,4 @@ variable "node_groups" {
       instance_type    = "t2.micro"
     }
   }
-}
-
-variable "subnets" {
-  description = "List of subnet IDs for the EKS cluster"
-  type        = list(string)
-  default     = ["subnet-02b535d84f8ef94fb", "subnet-02b535d84f8ef94fb"]
 }
